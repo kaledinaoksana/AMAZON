3e3a1f44540043d65930d44aebfde8ee8a7216d6
‘kaledinaoa’ <kaledinaoa@gmail.com>
2023-08-13 23:37:49 +0400

    add db lib

---
+++ lib/DB.py
@@ -0,0 +1,67 @@
+# PROBLEMS
+# MovedIn20Warning: The ``declarative_base()`` function is now available as sqlalchemy.orm.declarative_base().
+#  (deprecated since: 2.0) (Background on SQLAlchemy 2.0 at: https://sqlalche.me/e/b8d9)
+# Base = declarative_base()
+# -------------------------
+from settings.settings import SQL
+
+from sqlalchemy import create_engine, Column, String, Float, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from contextlib import contextmanager
+
+class DBwork:
+    
+    Base = declarative_base()
+
+    class AmazonBook(Base):
+        __tablename__ = 'AmazonBooks'
+
+        ID = Column(Integer, primary_key=True, autoincrement=True)
+        Title = Column(String)
+        Price = Column(String)
+        Score = Column(String)
+
+    # CONNECTION
+    def conn_db(self):
+        connection_string = 'DRIVER={'+SQL.DRIVER+'};SERVER='+SQL.SERVER+';DATABASE='+SQL.DB+';UID='+SQL.USER+';PWD='+SQL.PSW+';'
+        engine = create_engine(f"mssql+pyodbc:///?odbc_connect={connection_string}")
+        return engine
+
+    # CREATE TABLE
+    def create_table(engine):
+        DBwork.Base.metadata.create_all(engine)
+        return
+
+    # CREATE SESSION
+    def create_session(engine):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
+
+    # ADD
+    def add_object(session,obj):
+        session.add(obj)
+        session.commit()
+        return
+    
+    # DELETE
+    def delete_object(session, obj):
+        session.delete(obj)
+        session.commit()
+        return
+    
+    # close session
+    @contextmanager
+    def session_scope(engine):
+        """Provide a transactional scope around a series of operations."""
+        session = DBwork.create_session(engine)
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
